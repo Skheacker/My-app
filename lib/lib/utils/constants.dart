d8777b16b4fcdbb0a3e7ae7141f0256925a8dbf2
Skheacker <sg332348@gmail.com>
2025-12-24 22:38:01 +0530

    Create constants.dart

---
+++ lib/lib/utils/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static const String admobAppId = 'ca-app-pub-6040852748531505~2345044036';
+  static const String bannerAdUnitId = 'ca-app-pub-6040852748531505/4047355559';
+  static const String interstitialAdUnitId = 'ca-app-pub-6040852748531505/4047355559'; // Placeholder
+  static const String rewardedAdUnitId = 'ca-app-pub-6040852748531505/4047355559'; // Placeholder
+}
