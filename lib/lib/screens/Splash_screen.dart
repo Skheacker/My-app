00627d2fd9461d883ab16830ea2e0af08fb734e9
Skheacker <sg332348@gmail.com>
2025-12-24 22:21:07 +0530

    Create Splash_screen.dart

---
+++ lib/lib/screens/Splash_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'onboarding_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => OnboardingScreen()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Earning App Splash')),
+    );
+  }
+}
